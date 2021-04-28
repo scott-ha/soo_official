e561af3f1936f172844673bddd12cd70fe2d04f1
scott-ha <haneul9209@gmail.com>
2021-04-28 16:43:14 +0900

    asdasd

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* Main Page */
+// /
 router.get('/', function(req, res, next) {
-  res.render('main/index', { title: 'Express' });
+  res.render('main/page');
+});
+
+/* Price Page */
+// /price
+router.get('/price', function(req, res, next) {
+  res.render('price/page');
+});
+
+/* Works Page */
+// /works
+router.get('/works', function(req, res, next) {
+  res.render('works/page');
+});
+
+/* Works Page */
+// /works
+router.get('/solution', function(req, res, next) {
+  res.render('solution/page');
+});
+
+/* Works Page */
+// /works
+router.get('/contents', function(req, res, next) {
+  res.render('contents/page');
+});
+
+/* Works Page */
+// /works
+router.get('/solution', function(req, res, next) {
+  res.render('contents/page');
 });
 
 module.exports = router;
